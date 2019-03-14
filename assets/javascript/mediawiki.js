718e9264a0a178e19c713ea0b309fbba7ef96978
KaiAnnFletcher <kaiann.fletcher@mail.utoronto.ca>
2019-03-13 23:59:24 -0400

    correction

--- assets/javascript/mediawiki.js
+++ assets/javascript/mediawiki.js
@@ -5,7 +5,7 @@ function callWikiAPI (){
     
     
     // query currently hardcoded for Mona Lisa
-    var wikiQueryURL = "https://en.wikipedia.org/w/api.php"
+    var wikiQueryURL = "https://en.wikipedia.org/w/api.php?format=json&redirects=1&origin=*&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=" + searchWiki
     
     $.ajax({
         url: wikiQueryURL,
